eb2d6642e71340a32797ab41807df7f5177835bb
Luigi Del Vecchio <luigi.dv@hotmail.com>
2025-05-04 15:03:02 -0300

    funcionalidades basicas feitas, adicionado scraping para extração dos rosters dos times

---
+++ app.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from scraping import get_furia_valorant_roster, get_furia_cs_roster, get_furia_lol_roster
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/responder', methods=['POST'])
+def responder():
+    pergunta = request.json.get('pergunta')
+
+    if pergunta == "Qual o roster do time de Valorant da FURIA?":
+        resposta = get_furia_valorant_roster()
+    elif pergunta == "Qual o roster do time de CS2 da FURIA?":
+        resposta = get_furia_cs_roster()
+    elif pergunta == "Qual o roster do time de League of Legends da FURIA?":
+        resposta = get_furia_lol_roster()
+    elif pergunta == "Quando a FURIA joga?":
+        resposta = (
+            'Você pode acompanhar toda a agenda da FURIA pelo '
+            '<a href="https://www.hltv.org/team/8297/furia#tab-matchesBox" target="_blank">HLTV </a>'
+            '(CS2) '
+            'E a agenda da FURIA Valorant pelo '
+            '<a href="https://www.vlr.gg/matches" target="_blank">VLR.GG</a>'
+        )
+    
+    elif pergunta == "Qual o site oficial da FURIA?":
+        resposta = (
+            'Site oficial da FURIA: '
+            '<a href="https://www.furia.gg" target="_blank">furia.gg</a>'
+        )
+    else:
+        resposta = "Ainda não tenho informações sobre isso!"
+
+    return jsonify({"resposta": resposta})
---
+++ scraping.py
@@ -0,0 +1,79 @@
+from bs4 import BeautifulSoup
+
+def get_furia_valorant_roster():
+    try:
+        with open("web/valorant.html", "r", encoding="utf-8") as f:
+            content = f.read()
+
+        soup = BeautifulSoup(content, "html.parser")
+        nomes_reais = soup.find_all("div", class_="team-roster-item-name-real")
+        apelidos = soup.find_all("div", class_="team-roster-item-name-alias")
+
+        if len(nomes_reais) != len(apelidos):
+            return "Erro ao extrair dados."
+
+        resposta = "<strong>Roster Atual Valorant FURIA Brasileiro:</strong><br>"
+        for nome_real, apelido in zip(nomes_reais, apelidos):
+            resposta += f"- {nome_real.text.strip()} (<strong>{apelido.text.strip()}</strong>)<br>"
+
+        return resposta
+    except Exception as e:
+        return f"Erro ao ler o arquivo HTML: {str(e)}"
+    
+def get_furia_lol_roster():
+    try:
+        with open("web/lol.html", "r", encoding="utf-8") as f:
+            content = f.read()
+
+        soup = BeautifulSoup(content, "html.parser")
+        tabela_desejada = soup.find("table", class_=[
+        "wikitable",
+        "sortable",
+        "team-members",
+        "hoverable-rows",
+        "team-members-current",
+        "jquery-tablesorter"
+        ])
+        
+        if not tabela_desejada:
+            return "Tabela não encontrada."
+
+        nomes_reais = tabela_desejada.find_all("td", class_="team-members-irlname")
+        apelidos = tabela_desejada.find_all("td", class_="team-members-player")
+
+        if len(nomes_reais) != len(apelidos):
+            return "Erro ao extrair dados."
+
+        resposta = "<strong>Roster Atual League Of Legends FURIA Brasileiro:</strong><br>"
+        for nome_real, apelido in zip(nomes_reais, apelidos):
+            resposta += f"- {nome_real.text.strip()} (<strong>{apelido.text.strip()}</strong>)<br>"
+
+        return resposta
+    except Exception as e:
+        return f"Erro ao ler o arquivo HTML: {str(e)}"
+
+def get_furia_cs_roster():
+    try:
+        with open("web/cs2.html", "r", encoding="utf-8") as f:
+            content = f.read()
+
+        soup = BeautifulSoup(content, "html.parser")
+        tabela_desejada = soup.find("table", class_=[
+        "wikitable",
+        "wikitable-striped",
+        "roster-card"
+        ])
+        
+        nomes_reais = soup.find_all("div", class_="LargeStuff")
+        apelidos = soup.find_all("span", class_="inline-player")
+
+        if len(nomes_reais) != len(apelidos):
+            return "Erro ao extrair dados."
+
+        resposta = "<strong>Roster Atual Counter Strike 2 FURIA Brasileiro:</strong><br>"
+        for nome_real, apelido in zip(nomes_reais, apelidos):
+            resposta += f"- {nome_real.text.strip()} (<strong>{apelido.text.strip()}</strong>)<br>"
+
+        return resposta
+    except Exception as e:
+        return f"Erro ao ler o arquivo HTML: {str(e)}"
